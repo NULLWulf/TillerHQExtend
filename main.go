bea6bb10440a5cc4552b508d6aa64764012a021b
Nathan Wolf <ndwolf1991@gmail.com>
2024-08-28 10:23:05 -0400

    base file

--- main.go
+++ main.go
@@ -4,7 +4,8 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"os"
+	"strconv"
+	"strings"
 
 	"google.golang.org/api/option"
 	"google.golang.org/api/sheets/v4"
@@ -32,13 +33,21 @@ func main() {
 		log.Fatalf("Unable to retrieve data from sheet: %v", err)
 	}
 
+	var cumulativeSum float64
+
 	// Print the values from the response
 	if len(resp.Values) > 0 {
 		fmt.Println("Data from sheet:")
 		for i, row := range resp.Values {
-			fmt.Printf("%s, %s\n", row[0], row[2])
-			if i == 2 {
-				os.Exit(5)
+
+			// Remove dollar sign and any commas before parsing
+			valueStr := strings.ReplaceAll(row[2].(string), "$", "")
+			valueStr = strings.ReplaceAll(valueStr, ",", "")
+			// Attempt to parse row[2] as a number and add it to the cumulative sum
+			if num, err := strconv.ParseFloat(valueStr, 64); err == nil {
+				cumulativeSum += num
+			} else {
+				log.Printf("Error parsing value in row %d: %v", i+1, err) // Log the error but continue processing
 			}
 		}
 	} else {
